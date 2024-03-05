b4e3ce63d9c217217afb09b4b89a9231d42ee7b7
evertonrichard <evertonrichard444@gmail.com>
2024-03-05 11:55:09 -0300

    funcoes

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function recuperarTecla(tecla) {
+    document.getElementById('display').value += tecla;
+}
+
+function limparTecla() {
+    document.getElementById('display').value = '';
+}
+
+function calculo() {
     
 }
